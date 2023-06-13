17d1be2ac1a3685c210e73c7d2bdfbfaed507cfa
giovanyosorio <giovany.torres.799@gmail.com>
2023-06-13 12:37:35 -0500

    Reasignación y redeclaración

---
+++ src/scope/re.js
@@ -0,0 +1,34 @@
+var firstName //undefined
+firstName="gio"
+console.log(firstName);
+
+var lastname="david"
+
+lastname="diana"
+console.log(lastname);
+
+var secondName="david"
+var secondName="ana"
+console.log(secondName);
+
+
+//let 
+function name() {
+    let fruit="appe"
+    fruit="kiwi"
+    console.log(fruit);
+}
+name()
+
+let fruit ="banana"
+console.log(fruit);
+
+//const 
+
+const animal="dog"
+animal="cat"
+console.log(animal);
+
+const vehicles=[]
+vehicles.push("ferrari")
+console.log(vehicles);
