596fea97bd609356ab0b07f7f1ec7eab1a6e0275
giovanyosorio <giovany.torres.799@gmail.com>
2023-06-13 16:06:24 -0500

    hoisting

---
+++ src/hoisting/index.js
@@ -0,0 +1,19 @@
+/* console.log(nameofDog);
+
+var nameofDog="elmo" */
+
+nameofDog()
+
+function nameofDog() {
+    console.log(`el mejor perro es ${elmo}`);
+}
+
+var elmo="elmo"
+
+if (true){
+    var saludo = "hola"
+    let despedida = "chao"
+  }
+  
+  console.log(saludo)
+  console.log(despedida)
