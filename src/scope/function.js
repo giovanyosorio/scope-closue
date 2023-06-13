0d167515dae367f053ea718fa54542d327363243
giovanyosorio <giovany.torres.799@gmail.com>
2023-06-13 12:03:09 -0500

    Function Scope

---
+++ src/scope/function.js
@@ -0,0 +1,12 @@
+function greeting() {
+    let userName="Ana"
+    let surname="pepe"
+    console.log(userName);
+    if (userName==="Ana") {
+        console.log(`hello ${userName}`);
+        console.log(surname);
+    }
+}
+
+greeting()
+console.log(username);
