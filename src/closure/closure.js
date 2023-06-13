e33694719c2c745113a7bc9c21bc461b07726c66
giovanyosorio <giovany.torres.799@gmail.com>
2023-06-13 15:50:07 -0500

    create pet list closure

--- src/closure/closure.js
+++ src/closure/closure.js
@@ -10,3 +10,17 @@ const g= greeting()
 
 console.log(g);
 console.log(g());
+
+
+function createPetList() {
+    const petList = [];
+    return function addPet(myPet) {
+      if (myPet) {
+        petList.push(myPet);
+      }
+      return petList;
+    }
+  }
+
+  const pet=createPetList()
+console.log(pet("rino"));
